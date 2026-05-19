86d75ddaf1fc63bf32058e22b2a849a86c020ef9
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-19 17:22:06 +0530

    Format TestRunner.java for consistency

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,8 +17,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"rerun:target/failed-rerun.txt" },
 		tags = "@Mythily",
-		monochrome = true,
-	    
+		monochrome = true	    
 		)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
