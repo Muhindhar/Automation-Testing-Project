c8aae84c0dceb028a46c5e3e0634d7bda17c203c
Malavicka V <malavicka7@gmail.com>
2026-05-19 12:03:28 +0530

    Resolved conflict

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,30 +1,22 @@
 package runner;
 
-
 import io.cucumber.testng.CucumberOptions;
+
+import org.testng.annotations.DataProvider;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-
 		features = "src/test/resources/features/IPD/Search.feature",
-
-		features = "src\\test\\resources\\features\\FrontOffice\\FrontOfficeComplaint.feature",
-
-
 		glue = "definitions",
-		
-		plugin = {"pretty",
-				"html:target/cucumber-report.html",
-				"json:target/cucumber-report.json",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"rerun:target/failed-rerun.txt" },		
+		plugin = {"pretty", 
+						"html:target/cucumber-report.html",
+						"json:target/cucumber-report.json",
+						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+						"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+						"rerun:target/failed-rerun.txt"},
 		monochrome = true)
 
-
-
-public class TestRunner extends AbstractTestNGCucumberTests {
-
+public class TestRunner extends AbstractTestNGCucumberTests{
+	
 }
-
