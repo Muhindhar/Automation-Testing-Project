30aebe84d01f51203d499370a9b43f14626b8374
Muhindhar <muhindharsv27@gmail.com>
2026-05-19 12:45:37 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 
-		features = "src\\test\\resources\\features\\FrontOffice\\FrontOfcCallLog.feature",
+		features = "src\\test\\resources\\features\\Appointment\\Appointmentfilter.feature",
 
 		glue = "definitions",
 		
