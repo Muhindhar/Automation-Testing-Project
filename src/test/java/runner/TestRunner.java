36f09b3b2232e059f955e04b4ad70a0a715f791b
Muhindhar <muhindharsv27@gmail.com>
2026-05-19 17:13:25 +0530

    Update feature path in TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 
 
-		features = "src\\test\\resources\\features\\Appointment\\AppointmentReportDownload.feature",
+		features = "src\\test\\resources\\features",
 
 
 		glue = "definitions",
