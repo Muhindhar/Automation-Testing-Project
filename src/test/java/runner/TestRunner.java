aec358418edc3d811c7a14cc2d3c26a50f696145
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-19 16:41:33 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 
-		features = "src\\test\\resources\\features\\prescription\\edit_prescription.feature",
+		features = "src\\test\\resources\\features\\prescription\\delete_prescription.feature",
 		glue = "definitions",
 		
 		plugin = {"pretty",
