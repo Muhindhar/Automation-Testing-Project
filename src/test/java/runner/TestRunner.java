278574a2bf4ee3f86d011570e9f724e42e74c70a
Muhindhar <muhindharsv27@gmail.com>
2026-05-20 17:41:35 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,26 +1,32 @@
 package runner;
+
 import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
 @CucumberOptions(
-		features = "src\\test\\resources\\features",
-		glue = "definitions",
-		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "rerun:target/failed-rerun.txt" },
-				tags = "(@Muhindhar or @Vigneshwaran or @jerishwin or @Malavicka or @Mythily) and not (@Developing or @Bug or @UnderDevelopment or @develop)",
-		//tags = "@Addcall",
-		monochrome = true)
-
-
+        features = "src/test/resources/features",
+        glue = "definitions",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/failed-rerun.txt"
+        },
+        tags = "@Muhindhar",
+        // tags = "@Addcall",
+        monochrome = true
+)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-	@Override
-	@DataProvider
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
 
-	public Object[][] scenarios() {
-
-		return super.scenarios();
-	}
+        return super.scenarios();
+    }
 }
