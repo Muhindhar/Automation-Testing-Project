a354d8ad96994e3ddaeb7bb686d6d9c4b57ba387
Muhindhar <muhindharsv27@gmail.com>
2026-05-20 15:06:47 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,29 +1,35 @@
 package runner;
 
-import io.cucumber.testng.CucumberOptions;
-
 import org.testng.annotations.DataProvider;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-		features = "src/test/resources/features",
+        features = "src/test/resources/features",
 
-		glue = "definitions",
-		
-		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"rerun:target/failed-rerun.txt" },
-		
-		monochrome = true	    
-		)
+        glue = "definitions",
+
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/failed-rerun.txt"
+        },
+
+        monochrome = true
+)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-	@Override
-	@DataProvider(parallel = true)
-	public Object[][] scenarios() {
-		return super.scenarios();
-	}
+
+    @Override
+    @DataProvider
+
+    public Object[][] scenarios() {
+
+        return super.scenarios();
+    }
 }
