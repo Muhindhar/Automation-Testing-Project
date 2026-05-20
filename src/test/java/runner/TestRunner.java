f5cd909f3f36d1dcc29984a96baa06236fad4d9a
itzMalx <157216802+itzMalx@users.noreply.github.com>
2026-05-20 17:42:06 +0530

    Update feature path in TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src\\test\\resources\\features",
+		features = "src\\test\\resources\\features\\Certificate\\CertificateTemplate.feature",
 		glue = "definitions",
 		
 		plugin = {"pretty",
