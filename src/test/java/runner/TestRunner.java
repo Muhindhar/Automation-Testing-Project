1347b5fffc48dad6dd0e005fedfdd618001518b6
itzMalx <157216802+itzMalx@users.noreply.github.com>
2026-05-21 17:16:28 +0530

    Update Cucumber feature path and tags in TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src\\test\\resources\\features\\Certificate\\CertificateTemplate.feature",
+		features = "src/test/resources/features",
 		glue = "definitions",
 		
 		plugin = {"pretty",
@@ -15,7 +15,7 @@ import io.cucumber.testng.CucumberOptions;
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"rerun:target/failed-rerun.txt" },
-		tags = "@jerishwin",
+		tags = "@Malavicka",
 		monochrome = true)
 
 
