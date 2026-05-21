06193553c85f8f99593f93cbe6345fbe9ae8d06d
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-21 17:37:49 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,14 +3,14 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-		features = "src\\test\\resources\\features",
+		features = "src\\test\\resources\\features\\prescription\\add_prescription",
 		glue = "definitions",
 		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"rerun:target/failed-rerun.txt" },
 		monochrome = true,
-		tags = "not @UnderDevelopment and not @Bug and not @SkipVisitor"
+		tags = "@Mythily"
 		)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
