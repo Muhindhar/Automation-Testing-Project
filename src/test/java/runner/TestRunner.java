bc04753d5b528699528be9f911a85d748caef591
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-21 17:37:49 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,23 +3,16 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
+
 		features = "src/test/resources/features",
 		glue = "definitions",
 		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-<<<<<<< HEAD
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"rerun:target/failed-rerun.txt" },
-		monochrome = true,
-		tags = "not @UnderDevelopment and not @Bug and not @SkipVisitor"
-		)
-=======
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "rerun:target/failed-rerun.txt" },
 				tags = "(@Muhindhar or @Vigneshwaran or @jerishwin or @Malavicka or @Mythily) and not (@Developing or @Bug or @UnderDevelopment or @develop)",
 		//tags = "@Addcall",
 		monochrome = true)
 
->>>>>>> 66d126c (updated add complaint)
 
 
 public class TestRunner extends AbstractTestNGCucumberTests {
