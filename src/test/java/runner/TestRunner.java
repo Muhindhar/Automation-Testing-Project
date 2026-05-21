5b1c3c62a4ffd1189d73feb8a007408becd756ea
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-21 17:41:54 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,7 +3,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-		features = "src\\test\\resources\\features\\prescription\\add_prescription",
+		features = "src\\test\\resources\\features\\prescription\\add_prescription.feature",
 		glue = "definitions",
 		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
