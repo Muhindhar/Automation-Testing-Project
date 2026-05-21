944c9cd833ecbf8473b72c890cdacfcf8cdc69ed
Muhindhar <muhindharsv27@gmail.com>
2026-05-21 17:24:44 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.testng.CucumberOptions;
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				"rerun:target/failed-rerun.txt" },
 		monochrome = true,
-		tags = "not @UnderDevelopment and not @Bug"
+		tags = "not @UnderDevelopment and not @Bug and not @SkipVisitor"
 		)
 =======
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "rerun:target/failed-rerun.txt" },
