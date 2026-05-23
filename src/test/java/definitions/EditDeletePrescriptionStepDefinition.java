abc893a2c4ab9593b2613e4da6aadb4cb3c1bb73
Mythilysenthil <mythilsenthilp@gamil.com>
2026-05-23 19:20:32 +0530

    Modified edit prescription step definition and fixed ConfigReader initialization issue

--- src/test/java/definitions/EditDeletePrescriptionStepDefinition.java
+++ src/test/java/definitions/EditDeletePrescriptionStepDefinition.java
@@ -1,7 +1,6 @@
 package definitions;
 
 import org.testng.Assert;
-
 import actions.PrescriptionAction;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,8 +10,14 @@ import utilities.HelperClass;
 
 public class EditDeletePrescriptionStepDefinition {
 
-    private final PrescriptionAction prescriptionAction =new PrescriptionAction(DriverFactory.getDriver());
-    private static final String IPD_NUMBER = ConfigReader.getProperty("IPDno");
+    private PrescriptionAction prescriptionAction;
+    private String ipdNumber;
+
+    public EditDeletePrescriptionStepDefinition() {
+        ConfigReader.loadProperties();
+        this.ipdNumber = ConfigReader.getProperty("IPDno");
+        this.prescriptionAction = new PrescriptionAction(DriverFactory.getDriver());
+    }
 
     @When("clicks on View Prescription")
     public void clicks_on_view_prescription() {
@@ -28,14 +33,14 @@ public class EditDeletePrescriptionStepDefinition {
 
     @When("updates the prescription details")
     public void updates_the_prescription_details() {
-        HelperClass.logger.info("Updating prescription details for IPD: {}",IPD_NUMBER);
+        HelperClass.logger.info("Updating prescription details for IPD: {}", ipdNumber);
         prescriptionAction.updatePrescriptionDetails();
     }
 
     @Then("the prescription should be updated successfully")
     public void the_prescription_should_be_updated_successfully() {
         HelperClass.logger.info("Verifying prescription update");
-        Assert.assertTrue(prescriptionAction.isPrescriptionUpdated(),"Prescription update failed");
+        Assert.assertTrue(prescriptionAction.isPrescriptionUpdated(), "Prescription update failed");
     }
 
     @When("clicks on Delete")
@@ -47,20 +52,18 @@ public class EditDeletePrescriptionStepDefinition {
     @Then("a confirmation popup should be displayed")
     public void a_confirmation_popup_should_be_displayed() {
         HelperClass.logger.info("Verifying delete confirmation popup");
-        Assert.assertTrue( prescriptionAction.isDeleteConfirmationPopupDisplayed(),
-                "Delete confirmation popup not displayed");
+        Assert.assertTrue(prescriptionAction.isDeleteConfirmationPopupDisplayed(), "Delete confirmation popup not displayed");
     }
 
     @When("the user confirms deletion")
     public void the_user_confirms_deletion() {
-        HelperClass.logger.info("Confirming deletion for IPD: {}",IPD_NUMBER);
+        HelperClass.logger.info("Confirming deletion for IPD: {}", ipdNumber);
         prescriptionAction.confirmDelete();
     }
 
     @Then("the prescription should be deleted successfully")
     public void the_prescription_should_be_deleted_successfully() {
         HelperClass.logger.info("Verifying prescription deletion");
-        Assert.assertTrue(prescriptionAction.isPrescriptionDeletedSuccessfully(),
-                "Prescription deletion failed");
+        Assert.assertTrue(prescriptionAction.isPrescriptionDeletedSuccessfully(), "Prescription deletion failed");
     }
 }
