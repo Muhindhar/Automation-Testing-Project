6da58744c1b7f3f24f2edd7f3d4e30e2a493a4cf
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-23 20:40:02 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,24 +2,21 @@ package runner;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
 @CucumberOptions(
-
-		features = "src\\test\\resources\\features\\prescription\\delete_prescription.feature",
-
-		glue = "definitions",
-		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"rerun:target/failed-rerun.txt" },
-		monochrome = true,
-		tags = "@Mythily"
-	)
-
+        features = "src\\test\\resources\\features\\prescription\\delete_prescription.feature",
+        glue = "definitions",
+        plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/failed-rerun.txt" },
+        monochrome = true,
+        tags = "@Mythily and @DeletePrescription"
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
-
-	@Override
-	@DataProvider
-	public Object[][] scenarios() {
-		return super.scenarios();
-	}
+    @Override
+    @DataProvider
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
