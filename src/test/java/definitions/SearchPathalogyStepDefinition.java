04237281afe1283be5f5fd28fd2cae52c15b022a
Mythilysenthil <mythilsenthilp@gamil.com>
2026-05-23 22:42:51 +0530

    fix: move ConfigReader calls into methods to avoid null prop at init

--- src/test/java/definitions/SearchPathalogyStepDefinition.java
+++ src/test/java/definitions/SearchPathalogyStepDefinition.java
@@ -1,7 +1,4 @@
 package definitions;
-import java.time.Duration;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import actions.PathalogyAction;
 import io.cucumber.java.en.*;
@@ -9,12 +6,11 @@ import utilities.ConfigReader;
 import utilities.DriverFactory;
 import utilities.HelperClass;
 public class SearchPathalogyStepDefinition {
-    private final PathalogyAction pathoAction = new PathalogyAction(DriverFactory.getDriver());
-    private final String billNo = ConfigReader.getProperty("billNo");
-    private final String invalidBillNo = ConfigReader.getProperty("invalidBillNo");
+    private PathalogyAction pathoAction;
 
     @Given("the user clicks on the Sign In button")
     public void the_user_clicks_on_the_sign_in_button() {
+        pathoAction = new PathalogyAction(DriverFactory.getDriver());
         DriverFactory.getDriver().get("https://demo.smart-hospital.in/site/userlogin");
         HelperClass.logger.info("Clicking Sign In button");
         pathoAction.clickSignup();
@@ -36,16 +32,19 @@ public class SearchPathalogyStepDefinition {
     }
     @When("the user searches pathology reports by valid bill number")
     public void the_user_searches_pathology_reports_by_valid_bill_number() {
+        String billNo = ConfigReader.getProperty("billNo");
         HelperClass.logger.info("Searching pathology report with Bill Number: {}", billNo);
         pathoAction.searchreport(billNo);
     }
     @Then("only pathology report results should be displayed")
     public void only_pathology_report_results_should_be_displayed() {
+        String billNo = ConfigReader.getProperty("billNo");
         pathoAction.isRecDisplayed(billNo);
         HelperClass.logger.info("Validated pathology report for Bill Number: {}", billNo);
     }
     @When("the user searches pathology reports by invalid bill number")
     public void the_user_searches_pathology_reports_by_invalid_bill_number() {
+        String invalidBillNo = ConfigReader.getProperty("invalidBillNo");
         HelperClass.logger.info("Searching with invalid Bill Number: {}", invalidBillNo);
         pathoAction.invalidSearch(invalidBillNo);
     }
