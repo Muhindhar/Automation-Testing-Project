ee005064e5024c19b15a42db994546350d5abc2e
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-23 23:31:27 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+
         features = "src\\test\\resources\\features\\IPD\\IPDPatientDetails.feature",
+
         glue = "definitions",
         plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
