3efa0b5e0d5a1809e0a307d7e577fec0a14841b3
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-23 22:02:11 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed-rerun.txt" },
         monochrome = true,
-        tags = "@Mythily and @ValidPathologySearch"
+        tags = "@Mythily"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
