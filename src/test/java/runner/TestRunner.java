85c1f8a17fe37043637152fd0be3d6935ea73e60
Mythilysenthil <mythilsenthilp@gamil.com>
2026-05-23 22:42:51 +0530

    fix: move ConfigReader calls into methods to avoid null prop at init

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,14 +5,14 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src\\test\\resources\\features\\pathalogy\\search.feature",
+        features = "src\\test\\resources\\features\\IPD\\IPDPatientDetails.feature",
         glue = "definitions",
         plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed-rerun.txt" },
         monochrome = true,
-        tags = "not @Bug and not @UnderDevelopment"
+        tags = "@Malavicka"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
