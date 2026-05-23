46b6b0ee9ce9b0db4ccad09334cabfdd23e7a075
Mythilysenthil <mythilsenthilp@gamil.com>
2026-05-23 21:47:43 +0530

    fix: point runner to pathology search feature

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,24 +1,21 @@
-package runner;
+﻿package runner;
 import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-
-		features = "src\\test\\resources\\features\\prescription\\edit_prescription.feature",
-
-		glue = "definitions",
-		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"rerun:target/failed-rerun.txt" },
-		monochrome = true,
-		tags = "not @UnderDevelopment and not @Bug and not @Developing")
-
+        features = "src\\test\\resources\\features\\pathalogy\\search.feature",
+        glue = "definitions",
+        plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/failed-rerun.txt" },
+        monochrome = true,
+        tags = "@Mythily and @ValidPathologySearch"
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
-
-	@Override
-	@DataProvider
-	public Object[][] scenarios() {
-		return super.scenarios();
-	}
+    @Override
+    @DataProvider
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
