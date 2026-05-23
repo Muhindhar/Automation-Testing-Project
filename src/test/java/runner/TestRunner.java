85a6fea83ee1573c9d32895c58297cd43a0a4ad5
Mythilysenthil <mythilsenthilp@gamil.com>
2026-05-23 20:57:27 +0530

    fix: update runner for delete prescription

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,94 +1,24 @@
-[33mb5b14ed[m Merge branch 'main' into Mythily
-[33m190dbb7[m Update TestRunner.java
-[33m638d4bd[m Refactor scenarios method in TestRunner class
-[33mf763ef1[m Update feature path in Cucumber options
-[33m899b956[m Update TestRunner tags to exclude specific scenarios
-[33m93894c5[m unner
-[33m5b1c3c6[m Update TestRunner.java
-[33m0619355[m Update TestRunner.java
-[33m9390658[m Update TestRunner.java
-[33mc1875f9[m[33m ([m[1;31morigin/Vigneshwaran[m[33m)[m Merge branch 'main' into Vigneshwaran
-[33mb29b0cb[m Changed the tag and marked a feature as Bug
-[33m66d126c[m updated add complaint
-[33m179dfc7[m Conflicts Resolved
-[33ma354d8a[m Update TestRunner.java
-[33mdcc9ee9[m Update TestRunner.java
-[33m94a8483[m Update TestRunner.java
-[33m7e12981[m Merge branch 'main' into feature/appointment-module
-[33me36690c[m Updating latest code
-[33m8f21213[m pushing latest code
-[33mc2a863c[m Merge branch 'main' into Mythily
-[33mc8eb0e6[m Update Cucumber tags in TestRunner
-[33m093f17a[m pushing latest code
-[33m827f0da[m Merge pull request #56 from VIGNESHWARAN-USER/Malavicka
-[33m2d5947e[m Implement the log on prescription and pathalogy
-[33mcc1b0da[m ExcelUtil and CSV util fixed and Completed Add Credentials feature
-[33m333948d[m final code except --add visitor
-[33m72f6114[m final code except --add visitor
-[33m229c581[m added add visitor
-[33m5806819[m added add visitor
-[33me9ffa35[m updated the report download from appointment
-[33md251222[m Update feature path in TestRunner class
-[33m47d8a35[m conflicts resolved
-[33m563e974[m Update feature file path in TestRunner
-[33m86d75dd[m Format TestRunner.java for consistency
-[33m2d5b92a[m Fix annotation placement in TestRunner class
-[33m2592492[m Update TestRunner.java
-[33m919efc7[m Update TestRunner.java
-[33me5ce872[m Update TestRunner.java
-[33m7a5d230[m updated the report download from appointment
-[33ma992460[m Conflicts resolved
-[33m9cf1bba[m updated the report download from appointment
-[33m3de3664[m Update TestRunner.java
-[33maec3584[m Update TestRunner.java
-[33m23a79ea[m Update TestRunner.java
-[33m31f1d57[m Update TestRunner.java
-[33m3e159ed[m Update feature path in TestRunner configuration
-[33m9fbc1d5[m Added tags on feature file
-[33m1ee004d[m Update TestRunner.java
-[33mf9035e0[m complaint in front office
-[33m20940de[m report download front office
-[33m91f7949[m Resolved conflicts
-[33m698acc7[m Conflicts resolved
-[33m1a40149[m Errors resolved
-[33m89d7e25[m call log front office updated
-[33m624f0f2[m running download reports
-[33m5d36aeb[m Enhancement of prescription and pathology
-[33m26e94b8[m Update feature file path in TestRunner
-[33m703b79d[m I modify the test runner for valid search scenario
-[33m2555a7a[m added updated file
-[33mfb33757[m Edited front office report download feature
-[33mf6a7a9e[m added dataset for AddConsultation
-[33m6c6e6cc[m Add consultation module edited, filenames changed and Prescription page added as stub
-[33m9066009[m Added Join consultation feature file and its step definition
-[33m01cc6c9[m changes
-[33mca9a623[m 66 tests pass
-[33m5e0632e[m Resolved conflicts
-[33mc613413[m Added Add prescription feature and its implementation
-[33me51abed[m Resolved conflicts
-[33m996642b[m updated locators in complaint FrontOffice
-[33m2991bad[m Added invalid payment feature on pathology
-[33m0e33e3f[m Merge branch 'main' into feature/appointment-module
-[33m0000821[m updating frontoffice
-[33m1e3ce10[m Merge branch 'main' into Jerishwin
-[33m04ce0f2[m Added scenario outline for search feature and corrected hooks
-[33m54e1f03[m Resolved conflicts
-[33m663d4c8[m Added search feature and its implementation
-[33mea21575[m Resolved Conflicts
-[33m31788eb[m Completed the profile epic whit its senarios
-[33m9bb1d7c[m added new features and locators updated
-[33m3b402bd[m Added 4 features
-[33ma394b60[m Added add prescription feature and its implementation
-[33m0d11b46[m adding 16-05
-[33md9bb7ec[m Framework converted to sequential execution
-[33mf646f09[m Conflicts resolved
-[33m66f2ecd[m Added ExtentManager and Modified POM.xml as per the standards
-[33m9e5167b[m Cucumber JSON report is added
-[33m0531e9f[m Target and Output folders added
-[33mf49d4bc[m upcoming
-[33m6602e0a[m Removed generated files and reports from tracking
-[33m4bf681e[m Added Search Feature in OPD Epic and Implimentaions
-[33m55654fb[m Added AddAppointment feature
-[33m9955b7b[m Added Login feature and Its Implementation
-[33mf11d00d[m updating file with passing invalid scenario
-[33m9e14d89[m[33m ([m[1;31morigin/feature/frontoffice-module[m[33m, [m[1;31morigin/feature/Certificate-module[m[33m)[m Project Skeleton Created
+package runner;
+import org.testng.annotations.DataProvider;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+@CucumberOptions(
+
+		features = "src\\test\\resources\\features\\prescription\\edit_prescription.feature",
+
+		glue = "definitions",
+		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+				"rerun:target/failed-rerun.txt" },
+		monochrome = true,
+		tags = "not @UnderDevelopment and not @Bug and not @Developing")
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+	@Override
+	@DataProvider
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
+}
