cfb16f1221c1adf6fe7ad629689fe5e0eedfd007
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-23 22:02:11 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,23 +5,20 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-
-		features = "src/test/resources/features",
-
-		glue = "definitions",
-		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"rerun:target/failed-rerun.txt" },
-		monochrome = true,
-		tags = "@Mythily and @DeletePrescription")
-
+        features = "src\\test\\resources\\features\\pathalogy\\search.feature",
+        glue = "definitions",
+        plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "rerun:target/failed-rerun.txt" },
+        monochrome = true,
+        tags = "not @Bug and not @UnderDevelopment"
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider
     public Object[][] scenarios() {
         return super.scenarios();
     }
-
 }
 
