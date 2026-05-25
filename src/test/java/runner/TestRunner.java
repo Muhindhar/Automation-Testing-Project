1ac990b8cb66911c4ff17a4030d409b9b067ed51
Muhindhar <muhindharsv27@gmail.com>
2026-05-25 13:53:40 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -13,14 +13,12 @@ import io.cucumber.testng.CucumberOptions;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed-rerun.txt" },
         monochrome = true,
-        tags = "@Mythily"
+        tags = "@Muhindhar"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
-
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
 }
-
