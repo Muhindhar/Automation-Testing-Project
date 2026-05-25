387c76470720c1f7983e57a67ee9abe809fc32c4
Muhindhar <muhindharsv27@gmail.com>
2026-05-25 13:53:40 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.testng.CucumberOptions;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "rerun:target/failed-rerun.txt" },
         monochrome = true,
-        tags = "not @UnderDevelopment and not @Bug"
+        tags = "@Muhindhar"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
