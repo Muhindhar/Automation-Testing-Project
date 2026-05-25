9b348c260cdd6f15ee5e0426bd4f94f144854fb0
Muhindhar <muhindharsv27@gmail.com>
2026-05-25 12:34:34 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,3 +1,35 @@
+// package runner;
+
+// import org.testng.annotations.DataProvider;
+
+// import io.cucumber.testng.AbstractTestNGCucumberTests;
+// import io.cucumber.testng.CucumberOptions;
+
+// @CucumberOptions(
+//         features = "src\\test\\resources\\features",
+//         glue = "definitions",
+//         plugin = {
+//                 "pretty",
+//                 "html:target/cucumber-report.html",
+//                 "json:target/cucumber-report.json",
+//                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+//                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+//                 "rerun:target/failed-rerun.txt"
+//         },
+//         monochrome = true,
+//         tags = "@Muhindhar and not @SkipVisitor"
+// )
+
+// public class TestRunner extends AbstractTestNGCucumberTests {
+
+//     @Override
+//     @DataProvider(parallel = true)
+//     public Object[][] scenarios() {
+
+//         return super.scenarios();
+//     }
+// }
+
 package runner;
 
 import org.testng.annotations.DataProvider;
@@ -6,24 +38,38 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src\\test\\resources\\features",
-        glue = "definitions",
+
+        features =
+        "src\\test\\resources\\features",
+
+        glue =
+        "definitions",
+
         plugin = {
+
                 "pretty",
+
                 "html:target/cucumber-report.html",
+
                 "json:target/cucumber-report.json",
+
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+
                 "rerun:target/failed-rerun.txt"
         },
-        monochrome = true,
-        tags = "@Muhindhar and not @SkipVisitor"
+
+        monochrome = true
 )
 
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner
+extends AbstractTestNGCucumberTests {
 
     @Override
+
     @DataProvider(parallel = true)
+
     public Object[][] scenarios() {
 
         return super.scenarios();
