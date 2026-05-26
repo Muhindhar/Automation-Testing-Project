6152f128a3832c4c816fab1b3bc45f1b116e06ff
Jerishwin Joseph <jerishwinj@gmail.com>
2026-05-26 16:22:24 +0530

    Added discharge feature

---
+++ src/test/java/definitions/OPD_DischargeDefinition.java
@@ -0,0 +1,19 @@
+package definitions;
+
+import io.cucumber.java.en.When;
+
+public class OPD_DischargeDefinition {
+	@When("clicks on the Discharge icon")
+	public void clicks_on_the_discharge_icon() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("fills in the discharge form")
+	public void fills_in_the_discharge_form() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
