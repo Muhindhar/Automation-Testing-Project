2320b7ed11682369b203b1cfd757035b7c080c8c
VIGNESHWARAN M <147700262+VIGNESHWARAN-USER@users.noreply.github.com>
2026-05-27 09:06:00 +0530

    Update Cucumber options in TestRunner class

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,27 +1,29 @@
 package runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+
 import io.cucumber.testng.CucumberOptions;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 
-        features = "src/test/resources/features",
+		features = "src\\test\\resources\\features",
+		glue = "definitions",
+		
+		plugin = {"pretty",
+				"html:target/cucumber-report.html",
+				"json:target/cucumber-report.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+				"rerun:target/failed-rerun.txt" },
+		monochrome = true,
+		tags = "not @UnderDevelopment and not @Bug"
+		)
 
-        glue = "definitions",
-
-        plugin = {
-                "pretty",
-                "html:target/cucumber-report.html",
-                "json:target/cucumber-report.json",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "rerun:target/failed-rerun.txt"
-        },
-        tags = "@Mythily",
-        monochrome = true
-)
 public class TestRunner extends AbstractTestNGCucumberTests {
-
-    @Override
+	@Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
