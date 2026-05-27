6455a9baa59d1965f7639fa22e6a2e211e70d381
Mythilysenthil <mythilysenthilp@gmail.com>
2026-05-27 06:24:23 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -16,7 +16,7 @@ import io.cucumber.testng.CucumberOptions;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failed-rerun.txt"
         },
-        tags = "not @Bug and not @UnderDevelopment",
+        tags = "@Mythily",
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
