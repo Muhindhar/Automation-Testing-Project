e415373f69684046a9aa3f6f424e1ab814108335
itzMalx <157216802+itzMalx@users.noreply.github.com>
2026-05-29 13:18:41 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src\\test\\resources\\features",
 
         features = "src\\test\\resources\\features\\IPD\\IPDPatientDetails.feature",
 
