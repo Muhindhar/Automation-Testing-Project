e8ca885ecb930035b62ffccb34ca20dd85d92e17
VIGNESHWARAN M <147700262+VIGNESHWARAN-USER@users.noreply.github.com>
2026-05-29 14:37:37 +0530

    Update feature file path in TestRunner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = "src\\test\\resources\\features\\IPD\\IPDPatientDetails.feature",
+        features = "src\\test\\resources\\features\\",
 
         glue = "definitions",
 
