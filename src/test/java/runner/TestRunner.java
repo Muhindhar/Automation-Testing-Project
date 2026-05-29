3cec75292fbf4dba88bd1698479c82aafea4f62e
itzMalx <157216802+itzMalx@users.noreply.github.com>
2026-05-29 14:47:27 +0530

    Refactor TestRunner configuration for clarity

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,44 +1,25 @@
 package runner;
-
 import org.testng.annotations.DataProvider;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-
         features = "src\\test\\resources\\features\\",
-
         glue = "definitions",
-
         plugin = {
-
                 "pretty",
-
                 "html:target/cucumber-report.html",
-
                 "json:target/cucumber-report.json",
-
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-
                 "rerun:target/failed-rerun.txt"
         },
-
         tags = "not @Bug and not @UnderDevelopment",
-
-        monochrome = true,
-
-        publish = true
+        monochrome = true
 )
-
 public class TestRunner extends AbstractTestNGCucumberTests {
-
     @Override
-
     @DataProvider(parallel = false)
-
     public Object[][] scenarios() {
-
         return super.scenarios();
     }
 }
