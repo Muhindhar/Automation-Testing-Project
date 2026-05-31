479063bde954fa45b2ee95312649422eb6e2443d
Muhindhar <muhindharsv27@gmail.com>
2026-05-31 22:39:34 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.testng.CucumberOptions;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failed-rerun.txt"
         },
-        tags = "@Muhindhar",
+        tags = "@Muhindhar and not @UnderDevelopment",
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
