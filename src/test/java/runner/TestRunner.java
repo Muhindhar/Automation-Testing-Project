5689f4adc498e5b4f2a49c6fa8c1acbe7c51f536
Muhindhar <muhindharsv27@gmail.com>
2026-05-31 10:15:29 +0530

    Updated locators in the bed status feature

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.testng.CucumberOptions;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failed-rerun.txt"
         },
-        tags = "not @Bug and not @UnderDevelopment",
+      tags = "not @Bug and not @UnderDevelopment",
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
