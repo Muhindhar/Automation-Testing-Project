d358ebc03f69c1e6eeabb1d298b18f2bc0d4294c
Muhindhar <muhindharsv27@gmail.com>
2026-06-01 22:55:29 +0530

    new feature notice

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src\\test\\resources\\features\\FrontOffice\\AddVisitorFrontOffice.feature",
+        features = "src\\test\\resources\\features\\Appointment\\NoticeBoard.feature",
         glue = "definitions",
         plugin = {
                 "pretty",
